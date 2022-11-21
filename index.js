47fd2f3943b1317d66e9f90bb408568d108eda4d
mbugua <mbuguafaimba@gmail.com>
2022-11-21 20:20:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400){
+    return("This one is on me!")
+  }
+  if(distance > 2000 && distance < 2500){
+    return("I will gladly take your thirty bucks.")
+  }
+  if(distance > 2500){
+    return("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === 'NYC'){
+    return("Ok, sounds good.")
+  }else{
+    return("No go.")
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(num){
   // Write your code here!
+  if(num === 'generous'){
+    return("Thank you so much.")
+  }
+  if(num === 'not as generous'){
+    return("Thank you.")
+  }else{
+    return("Bye.")
+  }
+
 }
